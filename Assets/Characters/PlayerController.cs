35890613b12d74e4c72ca368c7350c2cf82bef5e
Chloe <chloelcdev@gmail.com>
2022-07-21 14:38:41 -0400

    setup animation test in player controller

--- Assets/Characters/PlayerController.cs
+++ Assets/Characters/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerAnimState _currentAnimationState = PlayerAnimState.Idle;
+    Animator animator;
+
+    void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            SetAnimationState(PlayerAnimState.Idle);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SetAnimationState(PlayerAnimState.Walking);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            animator.SetTrigger("Interact");
     }
+
+    void SetAnimationState(PlayerAnimState state)
+    {
+        _currentAnimationState = state;
+        animator.SetInteger("AnimationState", (int)_currentAnimationState);
+    }
+}
+
+// Explicitly setting the values here to keep this from getting messed up in serialization if it's changed in weird ways later
+enum PlayerAnimState
+{
+    Idle = 0,
+    Walking = 1,
 }

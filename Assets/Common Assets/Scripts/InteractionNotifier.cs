5520757c8252d877e2e23c539183dc0092e9cfc0
Chloe <chloelcdev@gmail.com>
2022-07-21 18:16:06 -0400

    create some fade for the notifier so it looks pretty

--- Assets/Common Assets/Scripts/InteractionNotifier.cs
+++ Assets/Common Assets/Scripts/InteractionNotifier.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class InteractionNotifier : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class InteractionNotifier : MonoBehaviour
     List<Interactable> nearbyInteractables = new List<Interactable>();
 
     [HideInInspector] public Interactable closestInteractable = null;
+
+    [SerializeField] float messageFadeTime = 0.3f;
 
     private void Awake()
     {
@@ -61,33 +64,61 @@ public class InteractionNotifier : MonoBehaviour
         if (nearbyInteractables.Count == 0)
         {
             closestInteractable = null;
-            SetMessage("", false);
+            SetMessage("");
             return;
         }
 
         // find the closest interactable
 
         float closestDistance = 0;
+        Interactable closestInteractableFound = null;
 
         foreach (var interactable in nearbyInteractables)
         {
             float distance = Vector3.Distance(interactable.transform.position, transform.position);
-            if (distance < closestDistance || closestInteractable == null)
+            if (distance < closestDistance || closestInteractableFound == null)
             {
-                closestInteractable = interactable;
+                closestInteractableFound = interactable;
                 closestDistance = distance;
             }
             
         }
 
-        // set the message of the closest interactable
+        // if the closest interactable changed, update which one is closest and set its message
 
-        SetMessage(closestInteractable.message, true);
+        if (closestInteractable != closestInteractableFound)
+        {
+            closestInteractable = closestInteractableFound;
+
+            SetMessage(closestInteractable.message);
+        }
     }
 
-    void SetMessage(string text, bool enabled)
+    void SetMessage(string text)
     {
-        _notificationText.text = string.Format(startingTextFormat, text);
-        _notificationText.gameObject.SetActive(enabled);
+        bool enabled = false;
+
+        if (text != "")
+        {
+            enabled = true;
+            _notificationText.text = string.Format(startingTextFormat, text);
+        }
+
+        // do a nice little fade when we enable/disable it
+
+        if (enabled)
+        {
+            _notificationText.DOKill();
+            _notificationText.gameObject.SetActive(true);
+            _notificationText.DOFade(1, messageFadeTime).onComplete = () => { };
+        }
+        else if (!enabled)
+        {
+            _notificationText.DOKill();
+            _notificationText.DOFade(0, messageFadeTime).onComplete = () => 
+            {
+                _notificationText.gameObject.SetActive(false);
+            };
+        }
     }
 }

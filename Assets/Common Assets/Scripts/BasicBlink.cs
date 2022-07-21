69c3195b2f5713a7b3df6a38a8975dfc184f76df
Chloe <chloelcdev@gmail.com>
2022-07-21 17:22:45 -0400

    Create a world

---
+++ Assets/Common Assets/Scripts/BasicBlink.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BasicBlink : MonoBehaviour
+{
+    CanvasGroup _canvasGroup;
+    SpriteRenderer _spriteRenderer;
+
+    [SerializeField] float FadeTime = 1;
+
+    private void Start()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (_spriteRenderer != null)
+            _spriteRenderer.DOFade(0, 1).SetLoops(0, LoopType.Yoyo);
+        else if (_canvasGroup != null)
+            _spriteRenderer.DOFade(0, 1).SetLoops(0, LoopType.Yoyo);
+        else
+            Debug.LogWarning($"BasicBlink exists on object {gameObject.name} but no CanvasGroup or SpriteRenderer is set");
+    }
+}

4563464da6f1860cbe4be4522cc5379a1257dd8b
Chloe <chloelcdev@gmail.com>
2022-07-22 19:22:00 -0400

    create ui locator for canvas, separate uicanvas prefab

---
+++ Assets/Common Assets/Scripts/UILocator.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILocator : MonoBehaviour
+{
+    public static UILocator _instance;
+
+    public List<RectTransformStringPair> UIElements;
+
+    public Dictionary<string, RectTransform> UIDictionary;
+
+    private void Awake()
+    {
+        _instance = this;
+
+        UpdateUIDictionary();
+    }
+
+    void UpdateUIDictionary()
+    {
+        UIDictionary.Clear();
+
+        foreach (var pair in UIElements)
+        {
+            UIDictionary.Add(pair.name, pair.rectTranform);
+        }
+    }
+
+    public static RectTransform LocateUI(string name)
+    {
+        if (_instance.UIDictionary.ContainsKey(name))
+            return _instance.UIDictionary[name];
+        else
+            return null;
+    }
+}
+
+[System.Serializable]
+public struct RectTransformStringPair
+{
+    public string name;
+    public RectTransform rectTranform;
+}
+

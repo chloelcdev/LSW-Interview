00c1e509a4c780a33a540b0b8215ff34bfa1b604
Chloe <chloelcdev@gmail.com>
2022-07-22 13:41:16 -0400

    publicize money

--- Assets/Characters/PlayerInventoryScrob.cs
+++ Assets/Characters/PlayerInventoryScrob.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class PlayerInventoryScrob : ScriptableObject
 {
-    int money = 0;
+    public int money = 0;
     
     // put a list of clothing items we own here later
 }

caa899eb2d3d0c8903bacf9e690bde0fe71d2939
Chloe <chloelcdev@gmail.com>
2022-07-22 23:56:17 -0400

    add createitem and setitem to inventoryslot, along with currentitem and isShopInventorySlot

--- Assets/Common Assets/Scripts/InventorySlot.cs
+++ Assets/Common Assets/Scripts/InventorySlot.cs
@@ -6,8 +6,25 @@ public class InventorySlot : MonoBehaviour
 {
     [HideInInspector] public RectTransform rectTransform;
 
+    public InventoryItem currentItem = null;
+
+    public bool isShopInventorySlot = false;
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void CreateItem(InventoryItem itemPrefab, EquipmentScrob item)
+    {
+        var spawnedItem = Instantiate(itemPrefab, transform);
+        spawnedItem.Setup(item, this);
+
+        SetItem(spawnedItem);
+    }
+
+    public void SetItem(InventoryItem item)
+    {
+        currentItem = item;
     }
 }

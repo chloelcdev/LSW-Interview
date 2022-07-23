9b7d374827e7aca70971eb4fc597fb6d7235664a
Chloe <chloelcdev@gmail.com>
2022-07-23 13:42:47 -0400

    fix fade screen raycasting

--- Assets/ScreenFadeController.cs
+++ Assets/ScreenFadeController.cs
@@ -7,23 +7,27 @@ using DG.Tweening;
 public class ScreenFadeController : MonoBehaviour
 {
     [SerializeField] TMPro.TMP_Text thanksText;
+    Image image;
 
     private void Awake()
     {
-        Color col = GetComponent<Image>().color;
+        image = GetComponent<Image>();
+
+        Color col = image.color;
         col.a = 1;
-        GetComponent<Image>().color = col;
+        image.color = col;
     }
 
     private void Start()
     {
-        GetComponent<Image>().DOFade(0, 2);
+        image.DOFade(0, 2);
     }
 
     public void DoEndGame()
     {
         Debug.Log("endgame");
-        GetComponent<Image>().DOFade(1, 2).onComplete = () =>
+        image.raycastTarget = true;
+        image.DOFade(1, 2).onComplete = () =>
         {
             thanksText.DOFade(1,2);
         };

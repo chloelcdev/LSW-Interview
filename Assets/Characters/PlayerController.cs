53eac0c3fecda3fe057467e7f68360fe264a169e
Chloe <chloelcdev@gmail.com>
2022-07-23 06:22:25 -0400

    fix player movement not being constant

--- Assets/Characters/PlayerController.cs
+++ Assets/Characters/PlayerController.cs
@@ -81,6 +81,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
         ApplyDrag();
         ApplyVelocity();
     }

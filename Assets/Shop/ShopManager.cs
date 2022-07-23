69ae16815cb2dd0065d7174730d751e6b0e8b247
Chloe <chloelcdev@gmail.com>
2022-07-22 23:58:32 -0400

    start shopmanager class

---
+++ Assets/Shop/ShopManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    [SerializeField] PlayerInventoryScrob playerInventoryScrob;
+
+    [SerializeField] EquipmentDirectory equipmentDirectory;
+
+    [SerializeField] InventoryItem inventoryItemPrefab;
+
+    [SerializeField] List<InventorySlot> playerInventorySlots = new List<InventorySlot>();
+    [SerializeField] List<InventorySlot> shopInventorySlots = new List<InventorySlot>();
+
+    public void Populate()
+    {
+        foreach(var item in equipmentDirectory.GetPlayerEquipment())
+        {
+            AddItem(item, playerInventorySlots);
+        }
+
+        foreach (var item in equipmentDirectory.GetPlayerEquipment())
+        {
+            AddItem(item, shopInventorySlots);
+        }
+    }
+
+    void AddItem(EquipmentScrob item, List<InventorySlot> inventory)
+    {
+        foreach (var slot in inventory)
+        {
+            if (slot.currentItem == null)
+            {
+                slot.CreateItem(inventoryItemPrefab, item);
+            }
+        }
+    }
+
+    void Purchase(EquipmentScrob item)
+    {
+
+    }
+
+    void Sell(EquipmentScrob item)
+    {
+
+    }
+
+}
